04c730a177dc7bc54cb21b9f40c7988f3290c270
Danila <levafork@gmail.com>
2022-12-01 13:11:44 +0300

    Реализовал базовую логику взятия вопросов со stackoverflow по заданному тэгу

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from ya_uploader import YaUploader
 from settings import TOKEN
 
 if __name__ == '__main__':
+    # Задание 1
     set_of_heroes = Superheroes('Hulk', 'Captain America', 'Thanos')
     set_of_heroes.sort_by_intelligence()
 
+    # Задание 2
     path_to_file = 'C:\\Users\\levaf\\Downloads\\6863625.jpg'
     token = TOKEN
     uploader = YaUploader(token)
---
+++ stack_questions.py
@@ -0,0 +1,20 @@
+from time import time
+import requests
+
+# Двухдневный промежуток
+TIME_GAP = 172,800
+
+
+def get_tagged_questions(tag: str):
+    questions_link = 'https://api.stackexchange.com/2.3/questions'
+    today = time()
+    params = {
+        'site': 'stackoverflow',
+        'sort': 'activity',
+        'order': 'desc',
+        'tagged': tag,
+        'fromdate': today - TIME_GAP,
+        'todate': today,
+    }
+
+    return requests.get(questions_link, params=params).json()
